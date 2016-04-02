5a9106b8ba1c8baadd24a151a44bb785f51b8dda
Eugene Zhlobo <evgen.zhlobo@gmail.com>
2016-04-02 13:20:29 +0300

    Add possibility to build project

--- bin/cli.js
+++ bin/cli.js
@@ -7,12 +7,11 @@ argv.splice(0, 2);
 
 switch(argv[0]) {
   case "start":
-    process.stdout.write("Start, sir!\n");
     builder.watch();
     break;
 
   case "build":
-    process.stdout.write("Okay...\n");
+    builder.build();
     break;
 
   default:
--- builder.js
+++ builder.js
@@ -3,12 +3,8 @@ var path = require('path');
 
 var builder = {};
 
-builder.watch = function() {
-  var command = cp.spawn(path.join(process.cwd(), "node_modules", ".bin", "webpack"), [
-    "--watch",
-    "--colors",
-    "--config", path.join(path.resolve(__dirname), "webpack.config.js"),
-  ]);
+var executeCommand = function(command, argv) {
+  var command = cp.spawn(command, argv);
 
   command.stdout.on('data', function(message) {
     process.stdin.write(message);
@@ -17,6 +13,26 @@ builder.watch = function() {
   command.stderr.on('data', function(message) {
     process.stderr.write(message);
   });
+};
+
+var executeWebpackCommand = function(argv) {
+  return executeCommand(path.join(process.cwd(), "node_modules", ".bin", "webpack"), argv);
+};
+
+var pathToWebpackConfig = path.join(path.resolve(__dirname), "webpack.config.js")
+
+builder.watch = function() {
+  process.stdout.write("Start, sir!\n");
+  return executeWebpackCommand([
+    "--watch", "--colors", "--config", pathToWebpackConfig
+  ]);
+};
+
+builder.build = function() {
+  process.stdout.write("Okay...\n");
+  return executeWebpackCommand([
+    "-p", "--colors", "--config", pathToWebpackConfig
+  ]);
 };
 
 module.exports = builder;
