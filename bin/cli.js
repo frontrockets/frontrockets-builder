6bfb20c3f253c5ad2430f0104391b4fb6f2b7245
Eugene Zhlobo <evgen.zhlobo@gmail.com>
2016-04-02 13:24:23 +0300

    Use start command as a default

--- bin/cli.js
+++ bin/cli.js
@@ -6,14 +6,11 @@ var argv = process.argv;
 argv.splice(0, 2);
 
 switch(argv[0]) {
-  case "start":
-    builder.watch();
-    break;
-
   case "build":
     builder.build();
     break;
 
+  case "start":
   default:
-    process.exit()
+    builder.watch();
 }
